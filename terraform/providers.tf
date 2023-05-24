554d17cf30e99d2069c069e4df02958340ff08af
Kate Mun <55422642+onlywhale@users.noreply.github.com>
2023-05-24 11:03:32 +0900

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name   = "tfstate"
-    storage_account_name  = "tfstate20470"
+    storage_account_name  = "tfstate20473"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
